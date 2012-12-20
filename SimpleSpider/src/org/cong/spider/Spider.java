747bcd53343f04ad3089e25c5e52832bf24052c6
onionsheep <onion_sheep@163.com>
2012-12-20 13:58:42 +0800

    a simple spider for the complex network, analysis the relationships in the links of some web pages

---
+++ SimpleSpider/src/org/cong/spider/Spider.java
@@ -0,0 +1,8 @@
+package org.cong.spider;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Spider {
+  private static Logger logger = LogManager.getLogger(Spider.class);
+}
---
+++ SimpleSpider/src/org/cong/spider/SpiderUtils.java
@@ -0,0 +1,8 @@
+package org.cong.spider;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class SpiderUtils {
+  private static Logger logger = LogManager.getLogger(SpiderUtils.class);
+}
